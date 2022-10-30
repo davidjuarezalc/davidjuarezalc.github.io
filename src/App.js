80066d357c7ad6874748b16136aaa9f5595b6f2e
davidjuarezalc <47382655+davidjuarezalc@users.noreply.github.com>
2022-10-30 12:39:05 +0100

    First react commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import About from "./About/About";
+import "./App.css";
+import Career from "./Career/Career";
+
+function App() {
+  return (
+    <div className="App">
+      <About></About>
+      <Career></Career>
+    </div>
+  );
+}
+
+export default App;
