4a81f00fe79bf373f68aa5b310d35b1c3e64d420
davidjuarezalc <47382655+davidjuarezalc@users.noreply.github.com>
2022-11-01 10:41:44 +0100

    Create Contact section and move to sass files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import About from "./About/About";
 import "./App.scss";
 import Career from "./Career/Career";
+import Contact from "./Contact/Contact";
 import Footer from "./Footer/Footer";
 
 function App() {
@@ -9,6 +10,8 @@ function App() {
       <div className="content">
         <About />
         <Career />
+        <Contact />
+        <h2>¡En desarrollo!</h2>
       </div>
       <Footer />
     </div>
